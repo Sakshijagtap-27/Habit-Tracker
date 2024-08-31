1a40cb380c7f24a9060d853c37b2ff03eb293956
Sakshijagtap-27 <21104082.sakshi.jagtap@gmail.com>
2024-08-31 21:55:52 +0530

    dockerfile added

--- habbit-tracker/src/index.js
+++ habbit-tracker/src/index.js
@@ -1,13 +1,14 @@
-import ReactDOM from "react-dom";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-
 import HabbitProvider from "./store/HabbitProvider";
 import App from "./App";
 
-ReactDOM.render(
-	<HabbitProvider>
-		<App></App>
-	</HabbitProvider>,
-	document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <HabbitProvider>
+      <App />
+    </HabbitProvider>
+  </React.StrictMode>
 );
